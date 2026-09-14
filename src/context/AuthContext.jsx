d26da1f355c3b492c53aa5fcc6a39b06a8577bd2
Jaider Diosa <jaiderdiosa7@gmail.com>
2026-09-13 20:01:46 -0500

    arreglo para el login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,7 +11,6 @@ export function AuthProvider({ children }) {
     function iniciarSesion(nombre, contraseña) {
 
         nombre= nombre.trim();
-        contraseña = setContraseña.trim();
 
         if (nombre === "jaider" && contraseña === "2009") {
 
